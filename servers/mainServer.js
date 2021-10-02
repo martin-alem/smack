001bd300f0654ae6bd17b28f03947af04208a527
Martin Alemajoh <alemajohmartin@gmail.com>
2021-10-02 17:56:36 -0400

    complete login page design for desktop view

--- servers/mainServer.js
+++ servers/mainServer.js
@@ -1,14 +1,23 @@
 import express from "express";
+import favicon from "serve-favicon";
 import Server from "./Server.js";
-import Logger from "./../utils/Logger.js";
+import path from "path";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const options = {};
 const httpServer = new Server("http", options, app);
 
+app.use(favicon(path.join(__dirname, "../public", "/image/favicon-32x32.png")));
+app.use(favicon(path.join(__dirname, "../public", "/image/favicon-16x16.png")));
+app.use(express.static(path.join(__dirname, "../public")));
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
-  // Logger.log("INFO", new TypeError("Unknown type"), import.meta.url);
-  res.json({ message: "Main server up and running" });
+  res.render("login.ejs");
 });
 
 export default httpServer;
