1407453346765f651416357c37c0a6b5c3281a73
Martin Alemajoh <alemajohmartin@gmail.com>
2021-10-04 10:44:02 -0400

    implemented the context menu functiionality

--- public/js/page_functionality.js
+++ public/js/page_functionality.js
@@ -6,6 +6,7 @@ Author: Martin Alemajoh
 Data:   10/4/2021, 7:51:41 AM
 **/
 
+import ContextMenu from "./ContextMenu.js";
 
 // Tab functionality
 (function () {
@@ -25,8 +26,8 @@ Data:   10/4/2021, 7:51:41 AM
     if (tab["position"]) {
       const tabPosition = parseInt(tab["position"], 10);
       if (currentTabButtonPosition !== tabPosition) {
-        tabButtons[currentTabButtonPosition].firstElementChild ? tabButtons[currentTabButtonPosition].firstElementChild.classList.toggle("active") : tabButtons[currentTabButtonPosition].classList.toggle("active");
-        tabButtons[tabPosition].firstElementChild.classList.toggle("active");
+        tabButtons[currentTabButtonPosition].firstElementChild ? tabButtons[currentTabButtonPosition].firstElementChild.classList.toggle("chatAreaActive") : tabButtons[currentTabButtonPosition].classList.toggle("chatAreaActive");
+        tabButtons[tabPosition].firstElementChild.classList.toggle("chatAreaActive");
         tabContents[currentTabContentPosition].classList.toggle("hide");
         tabContents[tabPosition].classList.toggle("hide");
         currentTabButtonPosition = tabPosition;
@@ -34,4 +35,63 @@ Data:   10/4/2021, 7:51:41 AM
       }
     }
   }
+})();
+
+//Context Menus
+(function () {
+  let chatAreaActive = false;
+  let friendAreaActive = false;
+  let profileAreaActive = false;
+  let contextMenu = null;
+  const chatAreaContext = document.querySelector(".chat_room");
+  chatAreaContext.addEventListener("click", function (event) {
+    if (!chatAreaActive) {
+      chatAreaActive = true;
+      const target = event.target;
+      if (target.dataset["type"] === "msg_action") {
+        const context = target.parentElement;
+        contextMenu = new ContextMenu(context, "some data");
+        contextMenu = contextMenu.messageContextMenu(target.dataset["direction"]);
+      } else if (target.dataset["type"] === "conversion_msg") {
+        const context = target.parentElement;
+        contextMenu = new ContextMenu(context, "some data");
+        contextMenu = contextMenu.conversionContextMenu();
+      }
+    } else {
+      contextMenu.classList.add("hide");
+      chatAreaActive = false;
+    }
+  });
+
+  const friendAreaContext = document.querySelector(".tab_friends");
+  friendAreaContext.addEventListener("click", function (event) {
+    if (!friendAreaActive) {
+      friendAreaActive = true;
+      const target = event.target;
+      if (target.dataset["type"] === "friend_action") {
+        const context = target.parentElement;
+        contextMenu = new ContextMenu(context, "some data");
+        contextMenu = contextMenu.friendContextMenu();
+      }
+    } else {
+      contextMenu.classList.add("hide");
+      friendAreaActive = false;
+    }
+  });
+
+  const profileAreaContext = document.querySelector(".tab_profile");
+  profileAreaContext.addEventListener("click", function (event) {
+    if (!profileAreaActive) {
+      profileAreaActive = true;
+      const target = event.target;
+      if (target.dataset["type"] === "profile_action") {
+        const context = target.parentElement;
+        contextMenu = new ContextMenu(context, "some data");
+        contextMenu = contextMenu.profileContextMenu();
+      }
+    } else {
+      contextMenu.classList.add("hide");
+      profileAreaActive = false;
+    }
+  });
 })();
